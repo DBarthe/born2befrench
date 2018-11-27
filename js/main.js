791915524ef942e18c2ede87a1af479bf27ecb41
dbarthe <barthelemy.delemotte@gmail.com>
2018-11-27 22:26:16 +0100

    add translations

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 (function(){
-  alert("hello salut");
+
+  subtitleTranslations = [
+    "Born 2 Be French",
+    "Né-e pour être fancais-e",
+    "Geboren um französisch zu sein",
+    "Nacido para ser francés",
+    "ولد ليكون الفرنسية",
+    "Nato per essere francese",
+    "Nascido para ser francês",
+    "Geboren om Frans te zijn",
+    "Родился, чтобы быть французским",
+    "Född att vara fransk",
+    "Született francia",
+    "天生就是法国人"
+  ]
+
+  subtitle = subtitleTranslations[Math.floor(Math.random() * subtitleTranslations.length)];
+  alert(subtitle);
 })();
